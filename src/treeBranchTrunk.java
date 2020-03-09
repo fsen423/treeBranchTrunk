32d995ae777332121451c2d5fdaa658e97a314e3
91084579 <fsen423@gmail.com>
2020-03-09 13:28:13 -0700

    Thanks Becca

--- src/treeBranchTrunk.java
+++ src/treeBranchTrunk.java
@@ -2,6 +2,8 @@ public class treeBranchTrunk {
     public static void main(String[] args)
     {
         System.out.println("The trees are dying");
+        System.out.println("Mr. Beast planted 20,000,000 trees");
+        System.out.println("There are many types of trees");
     } //end main method ass
-    
+
 } //end driver class
